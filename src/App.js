b871c6ae5d50305ab127179c17757ffe1b230e6e
vyshnavi-adusumelli <vyshnavi.adusumelli@gmail.com>
2024-02-14 20:14:54 -0500

    Add Prompt Checker logic

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ const User = require('./model/User');
 const ListingGenerator = require('./util/ListingGenerator');
 const BillGenerator = require('./util/BillGenerator');
 const LoanConstants = require('./util/LoanConstants');
+const InputChecker = require('./util/InputChecker');
 
 class App {
   start() {
     console.log('\nWelcome to the Real Estate Application!');
-    const annualIncome = parseFloat(readlineSync.question('\nPlease enter your annual income: '));
+    const annualIncome = InputChecker.getNumericInput('\nPlease enter your annual income: ');
 
     const user = new User().setAnnualIncome(annualIncome);
 
@@ -20,9 +21,9 @@ class App {
     console.log(`Based on your income, the maximum loan amount you can afford is: $${user.maxLoan.toFixed(2)}`);
 
     console.log("\nPlease enter the following details of your choice")
-    const squareFeet = readlineSync.question('Minimum square footage: ');
-    const bedrooms = readlineSync.question('Minimum number of bedrooms: ');
-    const bathrooms = readlineSync.question('Minimum number of bathrooms: ');
+    const squareFeet = InputChecker.getNumericInput('Minimum square footage: ');
+    const bedrooms = InputChecker.getNumericInput('Minimum number of bedrooms: ');
+    const bathrooms = InputChecker.getNumericInput('Minimum number of bathrooms: ');
     user.setRequirements(squareFeet, bedrooms, bathrooms);
 
     console.log("\nGenerating listings based on your requirements...");
@@ -31,7 +32,7 @@ class App {
         console.log(`${index + 1}: Price $${listing.price.toFixed(2)}, Size: ${listing.squareFeet} sqft, $${listing.pricePerSquareFeet.toFixed(2)} price/sqft, ${listing.bedrooms} bed, ${listing.bathrooms} bath`);
     });
 
-    const choice = readlineSync.question('\nEnter the number of the property you wish to purchase, or 0 to exit: ');
+    const choice = InputChecker.getNumericInput('\nEnter the number of the property you wish to purchase, or 0 to exit: ');
     const index = parseInt(choice);
     if (index === 0)
         process.exit();
